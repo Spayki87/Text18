4a870988e8190869b12d5c02cd17970a4be91b3c
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-07-12 13:36:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Консольное_выражение
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char openBracket = '(';
+            char closeBracket = ')';
+
+            int depth = 0;
+            int maxDepth = 0;
+
+            Console.WriteLine("Ведите скобочное вырожение:");
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == openBracket)
+                {
+                    depth++;
+
+                    if (depth > maxDepth)
+                    {
+                        maxDepth = depth;
+                    }
+                }
+                else if (text[i] == closeBracket)
+                {
+                    depth--;
+                }
+
+                if (depth < 0)
+                {
+                    break;
+                }
+            }
+
+            if (depth == 0)
+            {
+                Console.WriteLine("Скобочное вырожение верное.");
+                Console.WriteLine("Скобочная глубина - " + maxDepth);
+            }
+            else
+            {
+                Console.WriteLine("Скобочное вырожение не верное.");
+            }
+        }
+    }
+}
